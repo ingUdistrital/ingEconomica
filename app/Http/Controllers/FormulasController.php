4d192670bd3c072a5a230894cd0dfa823bb2bb26
Jaime Norato <jn901113jn@gmail.com>
2016-11-17 23:35:38 -0500

    formula 2 j=j*n

--- app/Http/Controllers/FormulasController.php
+++ app/Http/Controllers/FormulasController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class FormulasController extends Controller
 {
@@ -10,10 +9,20 @@ class FormulasController extends Controller
      * calcula el interes efectivo (i) apartir del interes nominal (j)
      * @param $j- interes nominal(en años)
      * @param $n- periodo de tiempo en el que se aplica el tinteres (mensual=>12,trimestral=>4,semestral=>2, etc..)
-     * @return float
+     * @return float - interes efectivo
      */
     public function interesEfectivo($j,$n){
         $i=$j/$n;/** interes efectivo(en meses) */
         return $i;
+    }
+    /**
+     * calcula el interes nominal (j) apartir del interes efectivo (i)
+     * @param $i- interes efectivo(en meses)
+     * @param $n- periodo de tiempo en el que se aplica el interes (mensual=>12,trimestral=>4,semestral=>2, etc..)
+     * @return float - interes nominal
+     */
+    public function interesNominal($i,$n){
+        $j=$i*$n;
+        return $j;
     }
 }
