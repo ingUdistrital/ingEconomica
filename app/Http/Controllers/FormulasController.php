5c79cd72887cc13ee22e1467af1eaca02b50e32c
Jaime Norato <jn901113jn@gmail.com>
2016-12-11 13:57:51 -0500

    organizacion de formulas

--- app/Http/Controllers/FormulasController.php
+++ app/Http/Controllers/FormulasController.php
@@ -9,54 +9,6 @@ class FormulasController extends Controller
     /*********INTERES**********/
 
     /**
-     * calcula el interes efectivo (i) apartir del interes nominal (j)
-     * @param $j - interes nominal(en años)
-     * @param $n - periodo de tiempo en el que se aplica el tinteres (mensual=>12,trimestral=>4,semestral=>2, etc..)
-     * @return float - interes efectivo
-     */
-    public function interesEfectivo($j, $n)
-    {
-        $i = $j / $n;
-        /** interes efectivo(en meses) */
-        //return $i;
-        $data = [
-            'i' => $i
-        ];
-        return view('home')->whit($data);
-    }
-
-    /**
-     * calcula el interes nominal (j) apartir del interes efectivo (i)
-     * @param $i - interes efectivo(en meses)
-     * @param $n - periodo de tiempo en el que se aplica el interes (mensual=>12,trimestral=>4,semestral=>2, etc..)
-     * @return float - interes nominal
-     */
-    public function interesNominal($i, $n)
-    {
-        $j = $i * $n;
-        return $j;
-    }
-
-    /**
-     * calcula el interes complejo
-     * @param $p - monto inversion
-     * @param $i - interes efectivo(en meses)
-     * @param $n - periodo de tiempo en el que se vence la inversion.
-     * @param $s - monto final a la entrega del vencimiento
-     * @return float - interes complejo
-     */
-    public function interesComplejo($p, $i, $n, $s)
-    {
-        if (empty($s) || isset($s) || is_null($s)) {
-            $s = $p * (pow((1 + $i), $n));
-            return $s;
-        } elseif (empty($p) || isset($p) || is_null($p)) {
-            $p = $s / (pow((1 + $i), $n));
-            return $p;
-        }
-    }
-
-    /**
      * Convierte la tasa nominal a tasa efectiva
      * @param $j - tasa nominal
      * @param $m - periodo de tiempo (meses, trimestre,semestre, etc)
@@ -95,12 +47,45 @@ class FormulasController extends Controller
         return $i2;
     }
 
+    /**
+     * calcula el interes compuesto
+     * @param $p- monto inversion
+     * @param $i- tasa efectivo(en meses)
+     * @param $n- periodo de tiempo en el que se vence la inversion.
+     * @param $s- monto final a la entrega del vencimiento
+     * @return float - interes complejo
+     */
+    public function interesCompuesto($p, $i, $n, $s)
+    {
+        if (empty($s) || isset($s) || is_null($s)) {
+            $s = $p * (pow((1 + $i), $n));
+            return $s;
+        } elseif (empty($p) || isset($p) || is_null($p)) {
+            $p = $s / (pow((1 + $i), $n));
+            return $p;
+        }
+    }
+    /**
+     * calcula el monto de invercion (p), teniendo el monto final s al termino del periodo de tiempo total,
+     * esto se calcula apartir de la formula de interes compuesto
+     * @param $s- monto final a la entrega del vencimiento
+     * @param $i- tasa efectivo(en meses)
+     * @param $n- periodo de tiempo en el que se vence la inversion.
+     * @return float - monto inversion
+    */
+    public function montoInvercion($s,$i,$n){
+        $p = $s/(pow((1+$i),$n));
+        return $p;
+    }
+
+
+
 
 
     /*********ANUALIDADES**********/
 
     public function anualidadesPerpetuas(){
-        
+
     }
 
 
