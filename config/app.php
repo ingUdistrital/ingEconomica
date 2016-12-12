10a755a1a34b124e5fab6eab47362b4fd5df1fa4
Jaime Norato <jn901113jn@gmail.com>
2016-12-11 19:06:20 -0500

    interes compuesto

--- config/app.php
+++ config/app.php
@@ -177,7 +177,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /**Provider de terceros**/
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -225,6 +226,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        /**aliases de terceros*/
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
